d0daa670aed85d1d7e5de8ef36b26169c44b9747
Andy Watt <andy.watt83@gmail.com>
2018-07-30 20:40:06 +0100

    Initial project setup

---
+++ src/crypto-algorithms.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace crypto_algorithms.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/crypto-algorithms.customSHA256/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace crypto_algorithms.customSHA256
+{
+    public class Class1
+    {
+    }
+}
